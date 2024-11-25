177c381f4b954eb47a53b36c853fdbca4f9dfa50
masayukid <masayukid@keio.jp>
2024-11-25 15:17:12 +0900

    [refactor] Linqで可読性向上

--- Assets/Scripts/CPUConfig.cs
+++ Assets/Scripts/CPUConfig.cs
@@ -1,14 +1,16 @@
+using System;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CPUConfig", menuName = "Game/CPUConfig")]
 public class CPUConfig : ScriptableObject
 {
-    [System.Serializable]
+    [Serializable]
     public class ModeSettings
     {
-        [SerializeField] private CPUMode _mode;     // モード名 (例: Easy, Normal, Hard)
-        [SerializeField] private float _moveSpeed;     // 横移動速度
-        [SerializeField] private float _rotationSpeed; // 回転速度
+        [SerializeField] private CPUMode _mode;         // モード (例: Easy, Normal, Hard)
+        [SerializeField] private float _moveSpeed;      // 横移動速度
+        [SerializeField] private float _rotationSpeed;  // 回転速度
 
         public CPUMode Mode => _mode;
         public float MoveSpeed => _moveSpeed;
@@ -20,12 +22,6 @@ public class CPUConfig : ScriptableObject
 
     public ModeSettings GetSettingsByMode(CPUMode mode)
     {
-        foreach (var settings in _modeSettings)
-        {
-            if (settings.Mode == mode)
-                return settings;
-        }
-        
-        return _modeSettings.Length > 0 ? _modeSettings[0] : null;
+        return _modeSettings.Single(settings => settings.Mode == mode);
     }
 }
