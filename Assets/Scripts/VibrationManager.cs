ed30a6c23a2fa168dc168bf63806e4378c5a7c50
masayukid <masayukid@keio.jp>
2024-12-28 21:59:48 +0900

    [fix] UnityRemote対応

--- Assets/Scripts/VibrationManager.cs
+++ Assets/Scripts/VibrationManager.cs
@@ -6,26 +6,12 @@ public static class VibrationManager
     {
         if (SystemInfo.supportsVibration)
         {
-            PlaySystemSound(1519);
             Vibrate(3);
         }
     }
 
-    // iOS設定
-#if UNITY_IOS && !UNITY_EDITOR
-    [DllImport ("__Internal")]
-    private static extern void _playSystemSound(int n);
-#endif
-
-    private static void PlaySystemSound(int n)
-    {
-#if UNITY_IOS && !UNITY_EDITOR
-            _playSystemSound(n);
-#endif
-    }
-
     // Android設定
-#if UNITY_ANDROID && !UNITY_EDITOR
+#if UNITY_ANDROID
     public static AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
     public static AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
     public static AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
@@ -33,7 +19,7 @@ public static class VibrationManager
 
     private static void Vibrate(long milliseconds)
     {
-#if UNITY_ANDROID && !UNITY_EDITOR
+#if UNITY_ANDROID
             vibrator.Call("vibrate", milliseconds);
 #endif
         if (milliseconds >= 1000)
