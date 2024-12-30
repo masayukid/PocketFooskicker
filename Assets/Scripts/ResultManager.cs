fc8afbbb625a52cac0bc95ffbe93fc73154260f7
kenshin0011 <nakanishi@social-db.co.jp>
2024-12-31 04:55:06 +0900

    [add] クラッカー演出

--- Assets/Scripts/ResultManager.cs
+++ Assets/Scripts/ResultManager.cs
@@ -7,6 +7,8 @@ public class ResultManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI resultMessage;
+    [SerializeField] private GameObject victoryParticlePrefab;
+    [SerializeField] private Canvas canvas;
 
     void Start()
     {
@@ -18,12 +20,48 @@ public class ResultManager : MonoBehaviour
         if (isSelfWinner)
         {
             resultMessage.text = "You Win !";
+            SpawnVictoryParticles();
         }
         else
         {
             resultMessage.text = "You Lose...";
             resultMessage.color = Color.red;
         }
+    }
+
+    private void SpawnVictoryParticles()
+    {
+        RectTransform canvasRect = canvas.GetComponent<RectTransform>();
+        float canvasWidth = canvasRect.rect.width;
+        float canvasHeight = canvasRect.rect.height;
+        Vector2 leftPosition = new Vector2(-canvasWidth / 2, canvasHeight / 8);
+        Vector2 rightPosition = new Vector2(canvasWidth / 8, canvasHeight / 8);
+        
+        InstantiateParticle(leftPosition, Quaternion.Euler(0, 90, 0));
+        InstantiateParticle(rightPosition, Quaternion.Euler(0, -90, 0));
+    }
+
+    private void InstantiateParticle(Vector2 anchoredPosition, Quaternion rotation)
+    {
+        GameObject particleEffect = Instantiate(victoryParticlePrefab, canvas.transform);
+
+        RectTransform rectTransform = particleEffect.GetComponent<RectTransform>();
+
+        if (rectTransform != null)
+        {
+            rectTransform.anchoredPosition = anchoredPosition;
+            rectTransform.localRotation = rotation;
+            rectTransform.localScale = Vector3.one;
+        }
+
+        ParticleSystem ps = particleEffect.GetComponent<ParticleSystem>();
+
+        if (ps != null)
+        {
+            ps.Play();
+        }
+
+        Destroy(particleEffect, 5.0f);
     }
 
     public void OnSelect(string sceneName)
