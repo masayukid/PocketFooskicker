fe1af3a4a4515deb314fadfb74b9386f9779b14f
kenshin0011 <nakanishi@social-db.co.jp>
2025-01-04 00:38:29 +0900

    [ADD] Menu音

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private AudioSource _bgmAudioSource;
+    [SerializeField] private AudioSource _seTapAudioSource;
+
     public void OnSelect(string difficulty)
     {
+        _seTapAudioSource.Play();
+        
         if (Enum.TryParse(difficulty, out CPUMode cpuMode))
         {
             var data = new Dictionary<string, object>
