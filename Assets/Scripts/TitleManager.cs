a6bb9195148c88beb0157552e7c4fff22cfbf78f
kenshin0011 <nakanishi@social-db.co.jp>
2025-01-04 00:07:14 +0900

    [ADD] TitleにBGM追加

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -3,11 +3,22 @@
 public class TitleManager : MonoBehaviour
 {
     [SerializeField] private Animation _titleAnim;
+    [SerializeField] private AudioSource _bgmAudioSource;
+    [SerializeField] private AudioSource _seTapAudioSource;
+
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        _bgmAudioSource.Play();
+    }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            _seTapAudioSource.Play();
+
             if (_titleAnim.isPlaying)
             {
                 // アニメーション再生中ならスキップ
